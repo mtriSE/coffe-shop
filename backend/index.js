c51991882015864ac042d585943f8325bfc1c507
mtriSE <mtri0162@gmail.com>
2024-04-16 13:51:09 +0700

    set up back-end source

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config({ path: '.env' })
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: '*'
+}))
+
 app.get('/', (req, res) => {
-    res.status(200).json({ message: "hello world!" });
+    res.status(200).send('<h1>hello world</h1>');
 });
 
 app.listen(process.env.NODE_SERVER_PORT, () => {
