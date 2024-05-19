10fb5a382386ac7972e4a387a61cfe44d83d009b
mtriSE <mtri0162@gmail.com>
2024-05-20 00:49:41 +0700

    CRUD staff

--- backend/modules/staff/staff.controller.js
+++ backend/modules/staff/staff.controller.js
@@ -1,9 +1,70 @@
 const database = require('../database');
+const { updateRecordWithVariableFields, deleteRecordFromTable } = require('../../utils/index');
 
-exports.findById = async (req, res, next) => {
-    res.status(200).json({ message: `This controller function find staff by id ${req.params.id}` })
+exports.findAll = async () => {
+    try {
+        const sql = `select * from staff;`
+        const [staffs, fields] = await database.query(sql);
+        return staffs;
+    } catch (error) {
+        throw error;
+    }
 }
 
-exports.findAll = async (req, res, next) => {
-    res.status(200).json({ message: `This controller function find all staff` })
+exports.createOneStaff = async (branchId, body) => {
+    try {
+        const { Staff_CCCD, ...otherData } = body;
+        return {
+            branchId,
+            Staff_CCCD,
+            otherData
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.deleteStaff = async (staffId) => {
+    try {
+        return await deleteRecordFromTable({
+            tableName: 'staff',
+            identifiedColumns: ['Staff_CCCD'],
+            recordIdentifications: [staffId]
+        })
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.updateStaff = async (staffId, bodyUpdate) => {
+    try {
+        return await updateRecordWithVariableFields({
+            tableName: 'staff',
+            identifiedColumns: ['Staff_CCCD'],
+            recordIdentifications: [staffId],
+            bodyUpdate: bodyUpdate
+        });
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.findAllStaffOfBranch = async (branchId) => {
+    try {
+        const sql = `SELECT * FROM staff where Br_ID = ?`;
+        const [rows, fields] = await database.query(sql, [branchId]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.findById = async (staffId) => {
+    try {
+        const sql = ` select * from staff join branch on staff.Br_ID = branch.Br_ID join account on staff.Staff_CCCD = account.User_ID where staff.Staff_CCCD = ?;`;
+        const [rows, fields] = await database.query(sql, [staffId]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
 }
--- backend/modules/staff/staff.router.js
+++ backend/modules/staff/staff.router.js
@@ -1,7 +1,74 @@
 const express = require('express');;
 const router = express.Router();
 const controller = require('./staff.controller');
+const { sendSusccessResponse, handleErrorResponse } = require('../../utils/index');
 
-router.get('/', )
+// find all staff in all branches
+router.get('/', async (req, res) => {
+    try {
+        const staffs = await controller.findAll();
+        sendSusccessResponse(res, 200, 'Get all staff from all branches', staffs)
+    } catch (error) {
+
+        console.error(error);
+        res.status(500).json({
+            statusCode: 500,
+            message: error
+        });
+    }
+})
+
+/**
+ * @description: Register a new staff's account and create a new record in staff table
+*/
+router.post('/createStaff/:branchId', async (req, res) => {
+    try {
+        const newStaff = await controller.createOneStaff(req.params.branchId, req.body);
+        sendSusccessResponse(res, 200, 'Created staff', newStaff)
+    } catch (error) {
+        handleErrorResponse(res, 500, error);
+    }
+})
+
+// delete a staff
+router.delete('/deleteStaff/:staffId', async (req, res) => {
+    try {
+        const deletedStaff = await controller.deleteStaff(req.params.staffId);
+        sendSusccessResponse(res, 200, `Deleted staff with id ${req.params.staffId}`, deletedStaff);
+    } catch (error) {
+        handleErrorResponse(res, 500, error);
+    }
+})
+
+// update a staff
+router.put('/updateStaff/:staffId', async (req, res) => {
+    try {
+        const result = await controller.updateStaff(req.params.staffId, req.body);
+        sendSusccessResponse(res, 200, 'Updated staff', result);
+    } catch (error) {
+        handleErrorResponse(res, 500, error)
+    }
+})
+
+// find all staff in one branches
+router.get('/ofBranch/:branchId', async (req, res) => {
+    try {
+        const staffs = await controller.findAllStaffOfBranch(req.params.branchId);
+        sendSusccessResponse(res, 200, 'Found all staff in branch ' + req.params.branchId, staffs);
+    } catch (error) {
+        handleErrorResponse(res, 500, error);
+    }
+})
+
+// get details information about one staff
+router.get('/:staffId', async (req, res) => {
+    try {
+        const staff = await controller.findById(req.params.staffId);
+        sendSusccessResponse(res, 200, `Found staff with id ${req.params.staffId}`, staff);
+    } catch (error) {
+        handleErrorResponse(res, 500, error);
+    }
+})
+
 
 module.exports = router;
