c1e46720efe2491af30d4f4d573ef94d4b962791
mtriSE <mtri0162@gmail.com>
2024-05-26 10:29:07 +0700

    refactor code

--- frontend/src/routes/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Home = () => {
-  return <div>
-    <img src="img/banner.webp" alt="Homepage Banner" />
-  </div>;
-};
-
-export default Home;
--- frontend/src/routes/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Drinks } from "./Drinks";
-export { default as Rooms } from "./Rooms";
-export { default as Promos } from "./Promos";
-export { default as Branches } from "./Branches";
